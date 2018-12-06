d1fd02751a9ded87aca713aa018f781666fce4eb
Gordon <gordonmikkelsen87@gmail.com>
2018-12-06 12:07:00 +0100

    MainFragment createt

---
+++ app/src/main/java/com/easj/gord/tvapp/MainFragment.java
@@ -0,0 +1,24 @@
+package com.easj.gord.tvapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v17.leanback.app.BrowseFragment;
+import android.support.v17.leanback.widget.ArrayObjectAdapter;
+import android.support.v17.leanback.widget.HeaderItem;
+import android.support.v17.leanback.widget.ListRow;
+import android.support.v17.leanback.widget.ListRowPresenter;
+import android.support.v17.leanback.widget.OnItemViewClickedListener;
+import android.support.v17.leanback.widget.Presenter;
+import android.support.v17.leanback.widget.Row;
+import android.support.v17.leanback.widget.RowPresenter;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;;
+
+public class MainFragment extends BrowseFragment {
+}
